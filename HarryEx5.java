4b8dabb74a8749fcfda82029ef7cfb706fc43517
Deyvant R_K_Bhardwaj <58858873+DeyvWeb@users.noreply.github.com>
2022-01-24 19:05:59 +0530

    Add files via upload

---
+++ HarryEx5.java
@@ -0,0 +1,176 @@
+// Question 1 : What will be the output of this program: 
+
+// int a = 10;
+// if (a=11)
+//         System.out.println(“I am 11”);
+// else
+//         System.out.println(“I am not 11”);
+import java.util.Scanner;
+public class HarryEx5 {
+    public static void main(String[] args){
+    //     int a = 10;
+    // if (a=11)
+    // {
+    //     System.out.println(“I am 11”);
+
+    // }
+        
+    // else
+    // {
+    //     System.out.println(“I am not 11”);
+    // }
+        
+// Question 2: Write a program to find out whether a student is pass or fail; if it requires a total of 40% 
+// and at least 33% in each subject to pass. Assume 3 subjects and take marks as input from the user.
+
+    // Scanner sc = new Scanner(System.in);
+    // System.out.print("Enter Student Name : ");
+    // String str = sc.nextLine();
+
+    // System.out.print("Enter Physics Marks : ");
+    // double p = sc.nextDouble();
+
+    // System.out.print("Enter Chemistry Marks : ");
+    // double c = sc.nextDouble();
+
+    // System.out.print("Enter Mathematics Marks : ");
+    // double m = sc.nextDouble();
+
+    // double percentage = (0.33*(p+c+m)); 
+
+    // System.out.println("Thanks!" + str + " percentage is : " + percentage);
+
+    // if (percentage >= 40)
+    // {
+    //     if(p>= 33 && c >=33 && m>=33)
+    //     {
+    //         System.out.println("Pass");
+    //     }
+    //     else
+    //     {
+    //         System.out.println("Fail");
+    //     }
+    // }
+
+
+    
+// Question 3 :Calculate income tax paid by an employee to the government as per the slabs mentioned below:
+
+// Income Slab	Tax
+// 2.5L – 5.0L  	5%
+// 5.0L – 10.0L 	20%
+// Above 10.0L	30%
+// Note that there is not tax below 2.5L. Take the input amount as input from the user.
+
+
+    // try (Scanner sc = new Scanner(System.in)) {
+    //     System.out.print("Enter User Name : ");
+    //     String str = sc.nextLine();
+
+    //     System.out.print("Enter your salary : ");
+    //     double salary = sc.nextDouble();
+
+    //     if(salary >= 250000 && salary <= 500000)
+    //     {
+    //      double revised = (salary - (salary*(0.05)));
+    //         System.out.print(str + " : Your tax bracket it 5% so your revised salary will be : " + revised);
+    //     }
+    //     else if (salary > 500000 && salary <= 1000000)
+    //     {
+    //         double revised = (salary - (salary*(0.20)));
+    //         System.out.print(str + " : Your tax bracket it 20% so your revised salary will be : " + revised);
+    //     }
+
+    //     else if (salary > 1000000)
+    //     {
+    //         double revised = (salary - (salary*(0.30)));
+    //         System.out.print(str +" : Your tax bracket it 30% so your revised salary will be : " + revised);
+    //     }
+    //     else
+    //     {
+    //         System.out.print("There's no tax bracket for you buddy! ");
+    //     }
+    // }
+
+// Question 4: Write a Java program to find out the day of the week given the number [1 for Monday, 2 for Tuesday … and so on!]
+
+
+        
+//     try (Scanner sc = new Scanner(System.in)) {
+//         System.out.print("Enter Number to find out the day of the week : ");
+//         byte str = sc.nextByte();
+
+//         if(str == 1)
+//         {
+//             System.out.print(str + " The day you opted for is : Monday ");
+//         }
+//         else if(str == 2)
+//         {
+//             System.out.print(str + "The day you for is : Tuesday");
+//         }
+        
+//         else if(str == 3)
+//         {
+//             System.out.print(str + "The day you for is : Wednesday");
+//         }
+        
+//         else if(str == 4)
+//         {
+//             System.out.print(str + "The day you for is : Thursday");
+//         }
+        
+//         else if(str == 5)
+//         {
+//             System.out.print(str + "The day you for is : Friday");
+//         }
+        
+//         else if(str == 6)
+//         {
+//             System.out.print(str + "The day you for is : Saturday");
+//         }
+        
+//         else 
+//         {
+//             System.out.print(str + "The day you for is : Sunday ");
+//         }
+// }
+
+// Question 5:Write a Java program to find whether a year entered by the user is a leap year or not.
+
+    // try (Scanner sc = new Scanner(System.in)) {
+    //     System.out.print("Enter User Name : ");
+    //      String str = sc.nextLine();
+
+    //      System.out.print("Enter year : ");
+    //      double yr = sc.nextDouble();
+
+    //      if(yr%400==0)
+    //      {
+    //          System.out.print("Leap Year");
+    //      }
+    //      else{
+    //          System.out.print("No Leap Year");
+
+    //      }
+// Question 6:Write a program to find out the type of website from the URL:
+
+// .com – commercial website
+// .org – organization website
+// .in – Indian website
+
+//     Scanner sc = new Scanner(System.in);
+//     System.out.print("Enter Website Name : ");
+//     String var = sc.nextLine();
+//     switch(var)
+//     {
+//         case  ".org" -> System.out.print("Commercial Website");
+//        case  ".com" -> System.out.print("Organization Website");
+
+//     case  ".in" -> System.out.print("Indian Website");
+
+//     }
+
+//     }
+// }
+
+
